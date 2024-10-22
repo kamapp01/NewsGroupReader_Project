ad3318430a50f0792069670c632bba8f7d86c3f4
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-10-22 10:09:34 +0200

    view created

---
+++ NewsGroupReader_Project/Presentation/View/ConnectionView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MVVM_ICOM_INOTIFY.Presentation.View;
+
+public partial class ConnectionView : UserControl
+{
+    public ConnectionView()
+    {
+        InitializeComponent();
+    }
+}
